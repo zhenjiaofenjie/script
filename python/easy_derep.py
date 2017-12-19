3719c0be6dce9a8b8bd68ca32bd4836ed09a1bfa
zhenjiaofenjie <wjingsjtu@gmail.com>
2017-12-19 11:13:29 -0500

    add easy_derep.py

---
+++ python/easy_derep.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse, textwrap
+import csv
+
+class Table(object):
+    def __init__(self):
+        self.table = []
+
+    def ReadTable(self, inputtable, column):
+        keylist = []
+        with open(inputtable, 'r') as i:
+            reader = csv.reader(i, dialect='excel-tab')
+            for linelist in reader:
+                key = []
+                for col in column:
+                    key.append(linelist[col-1])
+                if key in keylist:
+                    continue
+                self.table.append(linelist)
+                keylist.append(key)
+
+    def PrintTable(self, output):
+        with open(output, 'w') as o:
+            wr = csv.writer(o, delimiter= '\t')
+            wr.writerows(self.table)
+
+if __name__ == '__main__':
+    #read the arguments
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''
+        Delete duplicate rows based on selected columns
+        '''))
+    parser.add_argument('-i', '--input', type=str, help='Input table', action='store', required=True)
+    parser.add_argument('-c', '--columns', type=int, help='The index of the key column in the table', action='append', required=True)
+    parser.add_argument('-o', '--output', type=str, help='Output file name', required=True)
+    args = parser.parse_args()
+
+    table = Table()
+    table.ReadTable(args.input, args.column)
+    table.PrintTable(args.output)
